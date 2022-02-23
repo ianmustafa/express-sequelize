d1a4d28c5fc77a7d7a2abb6f591d075616ec32d1
Ian Mustafa <cuplizian@gmail.com>
2022-02-23 23:20:42 +0700

    Import model and insert data

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 6400;
 
+const { Person } = require('./models');
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello world!' });
+});
+
+app.post('/persons', async (req, res) => {
+  try {
+    const person = await Person.create({ name: 'Budi', address: 'Depok' });
+    res.json({ message: 'Success!', data: { personId: person.id }});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to create new person!' });
+  }
 });
 
 app.listen(port, () => {
